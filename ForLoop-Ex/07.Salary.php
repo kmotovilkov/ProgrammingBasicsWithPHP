77ca09b284b1c73cd8403f0cb991a86a576adab2
Kalin Motovilkov <monyllakk@abv.bg>
2019-04-08 15:43:56 +0300

    Create 07.Salary.php

---
+++ ForLoop-Ex/07.Salary.php
@@ -0,0 +1,26 @@
+<?php
+
+$n = intval(readline());
+$salary = intval(readline());
+
+for ($i = 1; $i <= $n; $i++) {
+    $type = readline();
+    switch ($type) {
+        case "Facebook":
+            $salary -= 150;
+            break;
+        case "Instagram":
+            $salary -= 100;
+            break;
+        case "Reddit":
+            $salary -= 50;
+            break;
+    }
+    if ($salary <= 0) {
+        echo "You have lost your salary.";
+        return;
+    }
+}
+echo $salary;
+
+?>
