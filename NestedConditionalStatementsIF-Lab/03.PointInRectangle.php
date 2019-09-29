9d35ff8d2682533351b73e2637ab670815412de5
Kalin Motovilkov <monyllakk@abv.bg>
2019-09-29 16:52:04 +0300

    Create 03.PointInRectangle.php

---
+++ NestedConditionalStatementsIF-Lab/03.PointInRectangle.php
@@ -0,0 +1,17 @@
+<?php
+
+$x1 = floatval(readline());
+$y1 = floatval(readline());
+$x2 = floatval(readline());
+$y2 = floatval(readline());
+$x = floatval(readline());
+$y = floatval(readline());
+
+if (($x >= $x1 && $x <= $x2) &&
+    ($y >= $y1 && $y <= $y2)) {
+    echo "Inside" . PHP_EOL;
+} else {
+    echo "Outside" . PHP_EOL;
+}
+
+
