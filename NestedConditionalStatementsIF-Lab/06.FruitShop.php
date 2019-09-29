0b519c714280d05d3d26320c02cf2817bce5e77f
Kalin Motovilkov <monyllakk@abv.bg>
2019-09-29 16:56:16 +0300

    Create 06.FruitShop.php

---
+++ NestedConditionalStatementsIF-Lab/06.FruitShop.php
@@ -0,0 +1,46 @@
+<?php
+
+$fruit = strtolower(readline());
+$day = strtolower(readline());
+$quantity = floatval(readline());
+$price = -1;
+
+if ($day == "monday" || $day == "tuesday" || $day == "wednesday" ||
+    $day == "thursday" || $day == "friday") {
+    if ($fruit == "banana") {
+        $price = 2.50;
+    } else if ($fruit == "apple") {
+        $price = 1.20;
+    } else if ($fruit == "orange") {
+        $price = 0.85;
+    } else if ($fruit == "grapefruit") {
+        $price = 1.45;
+    } else if ($fruit == "kiwi") {
+        $price = 2.70;
+    } else if ($fruit == "pineapple") {
+        $price = 5.50;
+    } else if ($fruit == "grapes") {
+        $price = 3.85;
+    }
+} else if ($day == "saturday" || $day == "sunday") {
+    if ($fruit == "banana") {
+        $price = 2.70;
+    } else if ($fruit == "apple") {
+        $price = 1.25;
+    } else if ($fruit == "orange") {
+        $price = 0.90;
+    } else if ($fruit == "grapefruit") {
+        $price = 1.60;
+    } else if ($fruit == "kiwi") {
+        $price = 3.00;
+    } else if ($fruit == "pineapple") {
+        $price = 5.60;
+    } else if ($fruit == "grapes") {
+        $price = 4.20;
+    }
+}
+if ($price >= 0) {
+    printf("%.2f", ($price * $quantity));
+} else {
+    echo "error";
+}
