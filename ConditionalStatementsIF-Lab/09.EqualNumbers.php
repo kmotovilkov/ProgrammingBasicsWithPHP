94fd340cecba8bd3811921552a779597e24c61c2
Kalin Motovilkov <monyllakk@abv.bg>
2019-10-06 23:12:58 +0300

    Create 09.EqualNumbers.php

---
+++ ConditionalStatementsIF-Lab/09.EqualNumbers.php
@@ -0,0 +1,12 @@
+<?php
+
+$num1 = intval(readline());
+$num2 = intval(readline());
+$num3 = intval(readline());
+if ($num1 == $num2 && $num2 == $num3) {
+    echo "yes" . PHP_EOL;
+} else {
+    echo "no" . PHP_EOL;
+}
+
+
