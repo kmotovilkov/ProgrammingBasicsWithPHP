03c205bfe139b8fb8ab03378af1436b76fb7a20e
Kalin Motovilkov <monyllakk@abv.bg>
2019-10-06 23:09:28 +0300

    Create 04.Number1...9ToText.php

---
+++ ConditionalStatementsIF-Lab/04.Number1...9ToText.php
@@ -0,0 +1,29 @@
+<?php
+
+$number = intval(readline());
+if ($number == 1) {
+    echo "one" . PHP_EOL;
+} else if ($number == 2) {
+    echo "two" . PHP_EOL;
+} else if ($number == 3) {
+    echo "three" . PHP_EOL;
+} else if ($number == 4) {
+    echo "four" . PHP_EOL;
+} else if ($number == 5) {
+    echo "five" . PHP_EOL;
+} else if ($number == 6) {
+    echo "six" . PHP_EOL;
+} else if ($number == 7) {
+    echo "seven" . PHP_EOL;
+} else if ($number == 8) {
+    echo "eight" . PHP_EOL;
+} else if ($number == 9) {
+    echo "nine" . PHP_EOL;
+} else {
+    echo "number too big" . PHP_EOL;
+}
+ 
+   
+
+
+
