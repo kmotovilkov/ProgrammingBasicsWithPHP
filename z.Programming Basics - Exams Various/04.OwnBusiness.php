cea0ccc5c20ef48a993f3f2e50cd0ed90f051606
Kalin Motovilkov <monyllakk@abv.bg>
2019-10-08 15:23:49 +0300

    Create 04.OwnBusiness.php

---
+++ z.Programming Basics - Exams Various/04.OwnBusiness.php
@@ -0,0 +1,31 @@
+<?php
+
+$shirochina = intval(readline());
+$dyljina = intval(readline());
+$visochina = intval(readline());
+$komanda = ;
+$svobodnoProstransvo = $shirochina  $dyljina  $visochina;
+$zaetoProstranstvo = 0;
+$broiKomputri = 0;
+$razlika = true;
+
+while (($komanda = readline()) != Done) {
+
+
+    $broiKomputri = intval($komanda);
+    $svobodnoProstransvo -= $broiKomputri;
+
+
+    if ($svobodnoProstransvo0) {
+        echo No more free space!You need . abs($svobodnoProstransvo) . Cubic meters more .;
+       $razlika = false;
+       break;
+    }
+
+}
+
+
+    if ($razlika == TRUE) {
+        echo $svobodnoProstransvo Cubic meters left .;
+    
+    }
