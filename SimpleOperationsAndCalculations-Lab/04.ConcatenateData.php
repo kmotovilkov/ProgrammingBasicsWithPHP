0149e8102ce86733d064aea3ce23d4c6f3760349
Kalin Motovilkov <monyllakk@abv.bg>
2019-10-08 14:58:08 +0300

    Create 04.ConcatenateData.php

---
+++ SimpleOperationsAndCalculations-Lab/04.ConcatenateData.php
@@ -0,0 +1,10 @@
+<?php
+
+$firstName = readline();
+$lastName = readline();
+$age = readline();
+$city = readline();
+
+$str = "You are $firstName $lastName, a $age-years old person from $city.";
+echo $str;
+
